74cece7b702505e710df8ee25ea01b3f39ef932b
Kevin Alcerro <kevin1alcerro@gmail.com>
2019-09-02 23:37:30 -0500

    test: New feed selection suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -100,10 +100,21 @@ $(function () {
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function () {
+        var old;
+        /* TODO: Write a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */beforeEach(function (done) {
+            loadFeed(0, function () {
+                old = $('.feed').html();
+                loadFeed(1, done);
+            });
+        });
 
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
+        it('is different', function () {
+            expect($('.feed').html()).not.toBe(old);
+        });
+    });
 
 }());
